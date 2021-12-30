323656e4baf2081002757147983c7c9cae0a65c5
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:07:46 +0530

    Add files via upload

---
+++ PC/Palindrome.java
@@ -0,0 +1,22 @@
+class Palindrome
+{
+ public static void main(String argu[])
+ {
+  int n=12321,d,rev=0,x;
+  x=n;
+  while(n!=0)
+  {
+   d=n%10;
+   rev=rev*10+d;
+   n=n/10;
+  }
+  if(rev==x)
+  {
+   System.out.println("The number is palindrome");
+  }
+  else
+  {
+   System.out.println("The number is not palindrome");
+  }
+ }
+}
