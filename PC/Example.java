8d9a0f3697ac7ff45f60a8c93b0fd718a08dbce9
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:06:18 +0530

    Add files via upload

---
+++ PC/Example.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Example
+{
+ public static void main(String args[])
+ {
+  int a,b;
+  Scanner c=new Scanner(System.in);
+  a=c.nextInt();
+  b=c.nextInt();
+  testclass t=new testclass();
+  t.sum(a,b);
+  System.out.println("s = "+t.s);
+ }
+}
+class testclass
+{
+ int s=5;
+ void sum(int a,int b)
+ {
+  int sum;
+  sum=a+b;
+  System.out.println("Sum = "+sum);
+ }
+}
