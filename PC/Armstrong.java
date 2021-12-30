a5368d76026f2a665ce470f1cc5c3dec598d94bc
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2021-12-30 20:05:57 +0530

    Add files via upload

---
+++ PC/Armstrong.java
@@ -0,0 +1,22 @@
+class Armstrong
+{
+ public static void main(String argu[])
+ {
+  int n=370,d,x,sum=0;
+  x=n;
+  while(n!=0)
+  {
+   d=n%10;
+   sum=sum + (d*d*d);
+   n=n/10;
+  }
+  if(sum==x)
+  {
+   System.out.println("The number is Armstrong");
+  }
+  else
+  {
+   System.out.println("The number is not Armstrong");
+  }
+ }
+}
