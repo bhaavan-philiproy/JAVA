a3c69aff46fde4036f46e65da8845a39abf36402
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2022-01-25 20:14:29 +0530

    Add files via upload

---
+++ Lab/Cycle 3/Employee.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Employee
+{
+ int empno;
+ String a;
+ double phno;
+ void read()
+ {
+  Scanner o=new Scanner(System.in);
+  System.out.println("Enter employee no : ");
+  empno=o.nextInt();
+  System.out.println("Enter the Name : ");
+  a=o.next();
+  System.out.println("Enter the phone number");
+  phno=o.nextDouble();
+ }
+ void display()
+ {
+  System.out.println("Employee id : "+empno);
+  System.out.println("Name : "+a);
+  System.out.println("Phone no : "+phno);
+ }
+ public static void main(String args[])
+ {
+  Employee e[]=new Employee[5];
+  for(int i=0;i<5;i++)
+  {
+   e[i]=new Employee();
+   e[i].read();
+  }
+  System.out.println("\n Data are :-");
+  for(int i=0;i<5;i++)
+  {
+   e[i].display();
+  }
+ }
+}
