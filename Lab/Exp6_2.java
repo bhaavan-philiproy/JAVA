43c378cb4d96a13bb2ca0fe94c1ac6e3c6583814
Bhaavan Philip Roy <96874838+bhaavan-philiproy@users.noreply.github.com>
2022-02-20 18:38:10 +0530

    Add files via upload

---
+++ Lab/Exp6_2.java
@@ -0,0 +1,23 @@
+class Exp6_2
+{
+ public static void main(String args[])
+ {
+  try
+  {
+   int b=100;
+   if(b<500)
+    throw new balanceException();
+  }
+  catch(balanceException e)
+  {
+   System.out.println(e);
+  }
+ }
+}
+class balanceException extends Exception
+{
+ balanceException()
+ {
+  System.out.print("Low balance ");
+ }
+}
